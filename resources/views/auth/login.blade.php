866e74a222f4afd34bc5c96d2e5c4ac802bd314f
phanduylinh2002 <phanlinh2652002@gmail.com>
2024-03-28 21:56:12 +0700

    login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -70,7 +70,7 @@
                   {{-- <div class="form-check form-switch d-flex align-items-center mb-3">
                     <input class="form-check-input" type="checkbox" id="rememberMe" checked>
                     <label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
-                  </div> --}}
+                  </div> --}}   
                   <div class="text-center">
                     <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Sign in</button>
                   </div>
